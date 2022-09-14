ac629c31442e366cd5923db66cc83ccfa8ec8595
pass7390 <banagajaycy09@gmail.com>
2022-09-14 12:12:59 +0800

    new commit

---
+++ MP01/test.cpp
@@ -0,0 +1,19 @@
+// Programmed by : Jaycy Ivan A. Bañaga
+// CEIT 03-302A
+// Sept 14, 2022
+
+#include <iostream>
+using namespace std;
+
+class GasPump {
+    private:
+        double liter;
+        double cost;
+        double total;
+        
+};
+
+
+int main() {
+
+};
